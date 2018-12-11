52120ced673fc0ba3b2d7265f2f96a59a2b535f7
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2018-12-11 11:59:47 +0900

    Add predicate sample

--- UITestIntroUITests/UITestIntroUITests.swift
+++ UITestIntroUITests/UITestIntroUITests.swift
@@ -10,15 +10,8 @@ import XCTest
 class UITestIntroUITests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDown() {
@@ -26,8 +19,12 @@ class UITestIntroUITests: XCTestCase {
     }
 
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    
+    func testPredicate(){
+        let app = XCUIApplication()
+        XCTAssertTrue(NSPredicate(format: "exists == 1").evaluate(with: app.buttons["table"]))
+        XCTAssertTrue(NSPredicate(format: "exists == 1").evaluate(with: app.buttons["webview"]))
+        XCTAssertTrue(NSPredicate(format: "exists == 0").evaluate(with: app.buttons["fuga"]))
+    }
 }
