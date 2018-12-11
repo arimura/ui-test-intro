090137612d386c316c667ae697c7570e8fdbeac4
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2018-12-11 09:35:32 +0900

    Initial Commit

---
+++ UITestIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITestIntro
+//
+//  Created by 有村 皓太郎 on 2018/12/11.
+//  Copyright © 2018年 hormiga6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
